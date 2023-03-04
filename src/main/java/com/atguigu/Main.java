8b534b58e31141f4ceb0021a88c372df67e31d68
lijian <lijian788@163.com>
2023-03-04 15:20:15 +0800

    first commit

---
+++ src/main/java/com/atguigu/Main.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author 李健
+ * @version 1.0
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
